6a57fd088480d854d5b99b0557d39f81ccf53075
Can <akifcansonmez@gmail.com>
2023-03-27 00:27:45 +0300

    newest

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Request, Form, HTTPException
+from fastapi.responses import RedirectResponse
+import uvicorn
+from fastapi.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
+from starlette.middleware.sessions import SessionMiddleware
+from routers import auth, newmeal, analytics, suggestions, home
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+
+app.add_middleware(SessionMiddleware, secret_key="secret_key")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.include_router(auth.router)
+app.include_router(newmeal.router)
+app.include_router(analytics.router)
+app.include_router(suggestions.router)
+app.include_router(home.router)
