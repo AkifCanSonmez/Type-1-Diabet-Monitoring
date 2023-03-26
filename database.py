80548ef9b30fb664cf13b07ef609824e0f28fdf6
Can <akifcansonmez@gmail.com>
2023-03-26 03:57:50 +0300

    edit

---
+++ database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+POSTRESQL_DATABASE_URL = "postgresql://fnqnngdy:1_uGTYjYqGSVNEiiPdfZYIR5NlU8djeU@castor.db.elephantsql.com/fnqnngdy"
+
+engine = create_engine(POSTRESQL_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
