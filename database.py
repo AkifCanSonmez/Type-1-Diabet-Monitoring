802402b76dd0990dc7d1142f785b2f97062e927f
AkifCanSonmez <78687240+AkifCanSonmez@users.noreply.github.com>
2023-03-27 09:55:14 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-POSTRESQL_DATABASE_URL = "postgresql://fnqnngdy:1_uGTYjYqGSVNEiiPdfZYIR5NlU8djeU@castor.db.elephantsql.com/fnqnngdy"
+POSTRESQL_DATABASE_URL = "****"
 
 engine = create_engine(POSTRESQL_DATABASE_URL)
 
